12da46c50150c1b9265867517f308c1c84bd10e0
AR2D2-Space <androide20.7@tutanota.com>
2019-12-03 11:36:41 -0600

    front 21

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = true
 Vue.use(BootstrapVue)
 
 
